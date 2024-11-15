d4dcdd3c2e95237476e92241352da6e8e0ef69d7
666Charles666 ZRC <147719423+666Charles666@users.noreply.github.com>
2024-11-15 21:07:39 +0800

    Update IngredientsController.java

--- _301BakeHouse/src/Controller/IngredientsController.java
+++ _301BakeHouse/src/Controller/IngredientsController.java
@@ -1,4 +1,33 @@
 package Controller;
 
+import model.HashTable;
+import model.Ingredient;
+
 public class IngredientsController {
-}
+
+    int size;
+    HashTable<Ingredient> ingredientHashTable;
+    public IngredientsController(int size){
+        this.size = size;
+        this.ingredientHashTable = new HashTable<>(size);
+    }
+
+    public boolean addIngredient(Ingredient ingredient){
+        if (ingredient == null){
+            return false;
+        }
+        ingredientHashTable.add(ingredient);
+        return true;
+    }
+    public void displayIngredient() {
+    ingredientHashTable.displayHashTable();
+    }
+
+//    public static void main(String[] args) {
+//        IngredientsController ingredientsController = new IngredientsController(10);
+//        System.out.println(ingredientsController.size);
+//        Ingredient i= new Ingredient("egg","heihei",100);
+//        ingredientsController.addIngredient(i);
+//        ingredientsController.displayIngredient();
+//    }
+    }
