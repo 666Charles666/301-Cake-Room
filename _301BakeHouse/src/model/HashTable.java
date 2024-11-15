df9de1117849db6678aec1d7b4260e701c5a1225
666Charles666 ZRC <147719423+666Charles666@users.noreply.github.com>
2024-11-15 21:06:40 +0800

    Update HashTable.java

--- _301BakeHouse/src/model/HashTable.java
+++ _301BakeHouse/src/model/HashTable.java
@@ -2,52 +2,59 @@ package model;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Scanner;
 
-public class HashTable<E> {
-    private final List<E>[] hashTable;
+public class HashTable<T> {
+    private  Object[] hashTable;
 
-    public HashTable(int size){
-        hashTable=new List[size];
-        for (int i = 0; i < size; i++) {
-            hashTable[i] = new LinkedList<>();
-        }
+    public HashTable(int size) {
+        hashTable = new Object[size];
     }
 
-    public int hushFunction(int key){
-        return Math.abs(key%hashTable.length);
-    }
-
-    public int add(E item){
-        int key = item.hashCode();
-        int index = hushFunction(key);
-        hashTable[index].add(item);
-        return index;
-    }
-    public void displayHashTable(){
+    public void displayHashTable() {
+        System.out.println("Hash Table (using Linear Probing)\n======================");
         for (int i = 0; i < hashTable.length; i++) {
-            System.out.print("Slot " + i + ": ");
-            for (E item : hashTable[i]) {
-                System.out.print(item + " -> ");
-            }
-            System.out.println("null");
+            System.out.println(i + ". " + hashTable[i]);
         }
     }
 
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        HashTable<String> myHashSC = new HashTable<>(10);
-//        System.out.println("please enter");
-//        while (true) {
-//            String data = scanner.nextLine();
-//            if (data.isEmpty()) {
-//                break;
-//            }
-//            int index = myHashSC.add(data);
-//            System.out.println("Inserted '" + data + "' at slot: " + index);
-//        }
-//
-//        System.out.println("Final hash table:");
-//        myHashSC.displayHashTable();
-//    }
+    public int hashFunction(T key) {
+        return key.hashCode() % hashTable.length; // Use hashCode() for any type
+    }
+
+    public int add(T item) {
+        int home = hashFunction(item);
+        int loc = home;
+
+        do {
+            if (hashTable[loc] == null) { // Slot is free
+                hashTable[loc] = item;
+                return loc;
+            } else { // Collision resolution
+                loc = (loc + 1) % hashTable.length; // Probe ahead by 1 with wraparound
+                System.out.println("Collision. Probing location " + loc + "...");
+            }
+        } while (loc != home); // While not returned to original home (i.e., hash table is full)
+
+        return -1; // Hash table is full
+    }
+
+    public static void main(String[] args) {
+        HashTable<Integer> h = new HashTable<>(10); // Example with Integer type
+        int data;
+
+        do {
+            System.out.print("Enter value: ");
+            data = new java.util.Scanner(System.in).nextInt();
+            if (data != 0) {
+                int loc = h.add(data);
+                if (loc != -1) {
+                    System.out.println("Item " + data + " stored at location " + loc);
+                } else {
+                    System.out.println("Error. Cannot store " + data + ". Table is full.");
+                }
+            }
+        } while (data != 0);
+
+        h.displayHashTable();
+    }
 }
