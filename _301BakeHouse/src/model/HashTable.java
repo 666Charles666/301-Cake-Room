4ce4fe25aebb70663e9bb6ee6ff5b9400694e5da
666Charles666 ZRC <147719423+666Charles666@users.noreply.github.com>
2024-11-18 14:10:44 +0800

    Update HashTable.java

--- _301BakeHouse/src/model/HashTable.java
+++ _301BakeHouse/src/model/HashTable.java
@@ -4,57 +4,56 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class HashTable<T> {
-    private  Object[] hashTable;
+    private final Object[] hashTable;
 
     public HashTable(int size) {
         hashTable = new Object[size];
     }
 
     public void displayHashTable() {
-        System.out.println("Hash Table (using Linear Probing)\n======================");
         for (int i = 0; i < hashTable.length; i++) {
-            System.out.println(i + ". " + hashTable[i]);
+            if (hashTable[i] == null){
+                System.out.println(i + ":" + hashTable[i]);
+            }else {
+                System.out.println(i + ":" + hashTable[i].toString());
+            }
         }
     }
 
     public int hashFunction(T key) {
-        return key.hashCode() % hashTable.length; // Use hashCode() for any type
+        return key.hashCode() % hashTable.length;
     }
 
     public int add(T item) {
         int home = hashFunction(item);
-        int loc = home;
+        int i = home;
 
         do {
-            if (hashTable[loc] == null) { // Slot is free
-                hashTable[loc] = item;
-                return loc;
-            } else { // Collision resolution
-                loc = (loc + 1) % hashTable.length; // Probe ahead by 1 with wraparound
-                System.out.println("Collision. Probing location " + loc + "...");
+            if (hashTable[i] == null) {
+                hashTable[i] = item;
+                System.out.println("add successful");
+                return i;
+            } else {
+                i = (i + 1) % hashTable.length;
+                System.out.println("Collision. Probing location " + i + "...");
             }
-        } while (loc != home); // While not returned to original home (i.e., hash table is full)
+        } while (i != home);
 
-        return -1; // Hash table is full
+        return -1;
     }
-
-    public static void main(String[] args) {
-        HashTable<Integer> h = new HashTable<>(10); // Example with Integer type
-        int data;
+    public boolean delete(T item) {
+        int home = hashFunction(item);
+        int i = home;
 
         do {
-            System.out.print("Enter value: ");
-            data = new java.util.Scanner(System.in).nextInt();
-            if (data != 0) {
-                int loc = h.add(data);
-                if (loc != -1) {
-                    System.out.println("Item " + data + " stored at location " + loc);
-                } else {
-                    System.out.println("Error. Cannot store " + data + ". Table is full.");
-                }
+            if (hashTable[i] != null && hashTable[i].equals(item)) {
+                hashTable[i] = null;
+                System.out.println("delete successfully");
+                return true;
             }
-        } while (data != 0);
-
-        h.displayHashTable();
+            i = (i + 1) % hashTable.length;
+        } while (i != home && hashTable[i] != null);
+        System.out.println("delete fault");
+        return false; // Item not found
     }
 }
