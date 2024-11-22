436f29774ce43039788ba08d32669c48981568e9
666Charles666 ZRC <147719423+666Charles666@users.noreply.github.com>
2024-11-22 20:18:46 +0800

    Update IngredientsController.java

--- _301BakeHouse/src/Controller/IngredientsController.java
+++ _301BakeHouse/src/Controller/IngredientsController.java
@@ -3,19 +3,25 @@ package Controller;
 import model.HashTable;
 import model.Ingredient;
 
+import java.util.Scanner;
+
 public class IngredientsController {
 
     int size ;
     HashTable<Ingredient> ingredientHashTable;
+    Scanner scanner = new Scanner(System.in);
     public IngredientsController(int size){
         this.size = size;
         this.ingredientHashTable = new HashTable<>(size);
     }
 
-    public void addIngredient(Ingredient ingredient){
-        if (ingredient == null){
+    public void addIngredient(String name,String textualDes,double calorie){
+
+        if (name == null && textualDes == null && calorie <0){
+            System.out.println("add fault");
             return;
         }
+        Ingredient ingredient = new Ingredient(name,textualDes,calorie);
         ingredientHashTable.add(ingredient);
     }
 
@@ -24,8 +30,14 @@ public class IngredientsController {
         ingredientHashTable.displayHashTable();
     }
 
-    public void deleteIngredient(Ingredient ingredient){
-        ingredientHashTable.delete(ingredient);
+    public void deleteIngredient(String ingredient){
+        for (int i = 0; i < size ;i++){
+            if (ingredientHashTable.get(i).getName().equals(ingredient)){
+                ingredientHashTable.delete(ingredientHashTable.get(i));
+                System.out.println("add successfully");
+            }
+            System.out.println("can't find the ingredient of this name");
+        }
     }
     public String search(String ingredientName){
         for (int i = 0 ;i < size;i++){
@@ -40,6 +52,16 @@ public class IngredientsController {
         return "search default";
     }
 
+    public void update(String name){
+        deleteIngredient(name);
+        System.out.println("please input ingredient's textualDes");
+        String textualDes = scanner.nextLine();
+        System.out.println("please input How many calorie it have");
+        double calorie = scanner.nextDouble();
+        addIngredient(name,textualDes,calorie);
+        System.out.println("add successfully");
+    }
+
 //    public static void main(String[] args) {
 //        IngredientsController ingredientsController = new IngredientsController(50);
 //        Ingredient i= new Ingredient("egg","heihei",100);
@@ -49,4 +71,5 @@ public class IngredientsController {
 //        ingredientsController.displayIngredient();
 //        ingredientsController.search("egg");
 //    }
+
 }
