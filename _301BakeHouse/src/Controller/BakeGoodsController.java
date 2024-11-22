23ac78a4d817c014fd14886913eabc8486f5d351
ysjaps229714 <147827758+ysjaps229714@users.noreply.github.com>
2024-11-22 19:29:46 +0800

    Update BakeGoodsController.java

--- _301BakeHouse/src/Controller/BakeGoodsController.java
+++ _301BakeHouse/src/Controller/BakeGoodsController.java
@@ -1,56 +1,48 @@
 package Controller;
-
 import model.BakeGoods;
 import model.HashTable;
-
 import java.util.List;
 
 public class BakeGoodsController {
     HashTable<BakeGoods> BakeGoodsTable;
-    int size;;
+    int size;
 
-    public BakeGoodsController(int size) {
-        this.size = size;
-        this.BakeGoodsTable = new HashTable<>(size);
+public BakeGoodsController(int size) {
+    this.size = size;
+    this.BakeGoodsTable = new HashTable<>(size);
 
+}
+public boolean addGoods(BakeGoods bakeGoods){
+    if (bakeGoods == null){
+        System.out.println("Error!");
+        return false;
     }
-    public boolean addGoods(BakeGoods bakeGoods){
-        if (bakeGoods == null){
-            System.out.println("Error!");
-            return false;
-        }
-        BakeGoodsTable.add(bakeGoods);
-        System.out.println("Good have added!");
-        return true;
-    }
+    BakeGoodsTable.add(bakeGoods);
+    System.out.println("Good have added!");
+    return true;
+}
 
-    public boolean deleteGoods(BakeGoods bakeGoods){
-        if(bakeGoods == null){
-            System.out.println("Error!");
-            return false;
-        }
-        BakeGoodsTable.delete(bakeGoods);
-        System.out.println("Good have deleted");
-        return true;
+public boolean deleteGoods(BakeGoods bakeGoods){
+    return BakeGoodsTable.delete(bakeGoods);
     }
 
-    public void displayGoods(){
-        BakeGoodsTable.displayHashTable();
-    }
-    public String searchGoods(String GoodName) {
-        for (int i = 0 ;i < size;i++){
-            BakeGoods item = BakeGoodsTable.get(i);
-            if (item != null && item.getName().equals(GoodName)) {
-                System.out.println(item.toString());
-                return item.toString();
-            }
-        }
-        return "Error Output";
-    }
-  public String updateGoods(String Goodsname,BakeGoods newgoods){
+public void displayGoods(){
+    BakeGoodsTable.displayHashTable();
+}
+public String searchGoods(String Goodsname) {
     for (int i = 0 ;i < size;i++){
         BakeGoods item = BakeGoodsTable.get(i);
-    if (Goodsname != null && item.getName().equals(Goodsname)) {
+        if (item != null && item.getName().equals(Goodsname)) {
+            System.out.println(item.toString());
+            return item.toString();
+}
+    }
+    return "Error Output";
+}
+public String updateGoods(String Goodsname,BakeGoods newgoods){
+    for (int i = 0 ;i < size;i++){
+        BakeGoods item = BakeGoodsTable.get(i);
+    if (item != null && item.getName().equals(Goodsname)) {
         item.setOriginCountry(newgoods.getOriginCountry());
         item.setTextualDES(newgoods.getTextualDES());
         item.setImgURL(newgoods.getImgURL());
@@ -60,17 +52,23 @@ public class BakeGoodsController {
     return ("fail to update");
 }
 
-//    public static void main(String[] args) {
-//        BakeGoodsController case1 = new BakeGoodsController(10);
-//        BakeGoods a = new BakeGoods("a","a","a","a");
-//        case1.addGoods(a);
-//        case1.displayGoods();
-//        case1.updateGoods("a",new BakeGoods("b","b","b","b"));
-//        case1.displayGoods();
-//        case1.searchGoods("a");
-//        case1.deleteGoods(a);
-//        case1.deleteGoods(a);
-//        case1.deleteGoods(a);
-//        case1.displayGoods();
-//    }
+    public static void main(String[] args) {
+        BakeGoodsController case1 = new BakeGoodsController(10);
+        BakeGoods a = new BakeGoods("a","a","a","a");
+        case1.addGoods(a);
+        case1.displayGoods();
+        case1.updateGoods("a",new BakeGoods("b","b","b","b"));
+        case1.displayGoods();
+        case1.searchGoods("a");
+        case1.deleteGoods(a);
+        case1.displayGoods();
+        case1.deleteGoods(a);
+        case1.displayGoods();
+    }
 }
+
+
+
+
+
+
