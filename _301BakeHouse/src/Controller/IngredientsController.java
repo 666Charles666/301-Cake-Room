c08e77b4d2be287d417b2bf60732768bb699d158
666Charles666 ZRC <147719423+666Charles666@users.noreply.github.com>
2024-11-23 13:37:57 +0800

    Update IngredientsController.java

--- _301BakeHouse/src/Controller/IngredientsController.java
+++ _301BakeHouse/src/Controller/IngredientsController.java
@@ -15,29 +15,44 @@ public class IngredientsController {
         this.ingredientHashTable = new HashTable<>(size);
     }
 
-    public void addIngredient(String name,String textualDes,double calorie){
+    public boolean addIngredient(String name,String textualDes,double calorie){
 
-        if (name == null && textualDes == null && calorie <0){
+        if (name == null || textualDes == null || calorie <0){
             System.out.println("add fault");
-            return;
+            return false;
         }
         Ingredient ingredient = new Ingredient(name,textualDes,calorie);
         ingredientHashTable.add(ingredient);
+        return true;
     }
 
-    public void displayIngredient() {
-        System.out.println("all of ingredients in there");
-        ingredientHashTable.displayHashTable();
-    }
-
-    public void deleteIngredient(String ingredient){
-        for (int i = 0; i < size ;i++){
-            if (ingredientHashTable.get(i).getName().equals(ingredient)){
-                ingredientHashTable.delete(ingredientHashTable.get(i));
-                System.out.println("add successfully");
+    public String displayIngredient() {
+        StringBuilder result = new StringBuilder("All ingredients in the hash table:\n");
+        for (int i = 0; i < size; i++) { // Assuming getSize() provides the size of hashTable
+            Ingredient ingredient = ingredientHashTable.get(i);
+            if (ingredient == null) {
+                result.append(i).append(": null\n");
+            } else {
+                result.append(i).append(": ").append(ingredient.toString()).append("\n");
             }
-            System.out.println("can't find the ingredient of this name");
         }
+        return result.toString();
+    }
+
+
+
+    public boolean deleteIngredient(String ingredient){
+        for (int i = 0; i < size ;i++){
+            Ingredient currentIngredient = ingredientHashTable.get(i);
+            if (currentIngredient != null && currentIngredient.getName().equals(ingredient)){
+                ingredientHashTable.delete(ingredientHashTable.get(i));
+//                System.out.println("delete successfully");
+                return true;
+            }
+
+        }
+        System.out.println("can't find the ingredient of this name");
+        return false;
     }
     public String search(String ingredientName){
         for (int i = 0 ;i < size;i++){
@@ -52,24 +67,34 @@ public class IngredientsController {
         return "search default";
     }
 
-    public void update(String name){
-        deleteIngredient(name);
-        System.out.println("please input ingredient's textualDes");
+    public boolean update(String name){
+//        deleteIngredient(name);
+        if (!deleteIngredient(name)){
+            System.out.println("update fault, can't find this ingredient");
+            return false;
+        }
+        System.out.println("please input the new ingredient's textualDes");
         String textualDes = scanner.nextLine();
         System.out.println("please input How many calorie it have");
         double calorie = scanner.nextDouble();
-        addIngredient(name,textualDes,calorie);
-        System.out.println("add successfully");
+        scanner.nextLine();
+        boolean added = addIngredient(name,textualDes,calorie);
+        if (added) {
+
+            return true;
+        }else{
+            System.out.println("Update failed,couldn't add updated ingredient.");
+            return false;
+        }
     }
 
-//    public static void main(String[] args) {
-//        IngredientsController ingredientsController = new IngredientsController(50);
-//        Ingredient i= new Ingredient("egg","heihei",100);
-//        ingredientsController.addIngredient(i);
-//        ingredientsController.displayIngredient();
-//        ingredientsController.addIngredient(new Ingredient("oil","nianhuhu",500));
-//        ingredientsController.displayIngredient();
-//        ingredientsController.search("egg");
-//    }
+    public static void main(String[] args) {
+        IngredientsController ingredientsController = new IngredientsController(50);
+        ingredientsController.addIngredient("香菜","好吃",12.6);
+        System.out.println(ingredientsController.displayIngredient());
+        ingredientsController.search("香菜");
+        ingredientsController.update("香菜");
+        System.out.println(ingredientsController.displayIngredient());
+    }
 
 }
