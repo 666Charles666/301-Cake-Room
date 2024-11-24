64f8770a7da56f15c8065ea80ef5111341604176
ysjaps229714 <147827758+ysjaps229714@users.noreply.github.com>
2024-11-24 12:13:25 +0800

    Update BakeGoodsController.java

--- _301BakeHouse/src/Controller/BakeGoodsController.java
+++ _301BakeHouse/src/Controller/BakeGoodsController.java
@@ -29,17 +29,59 @@ public boolean deleteGoods(BakeGoods bakeGoods){
 public void displayGoods(){
     BakeGoodsTable.displayHashTable();
 }
-    public BakeGoods search(String bakeGoodsname){
-        for (int i = 0 ;i < size;i++){
+    public BakeGoods[] searchByName(String bakeGoodsname) {
+        BakeGoods[] goods = new BakeGoods[size];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
             BakeGoods item = BakeGoodsTable.get(i);
 
             if (item != null && item.getName().equals(bakeGoodsname)) {
                 System.out.println("search successfully");
                 System.out.println(item.toString());
-                return item;
+                goods[count] = item;
+                count++;
             }
         }
-        return null;
+        for (int i = 1; i < count; i++) {
+            BakeGoods key = goods[i];
+            int j = i - 1;
+            while (j >= 0 && goods[j].getName().compareTo(key.getName()) > 0) {
+                goods[j + 1] = goods[j];
+                j--;
+            }
+            goods[j + 1] = key;
+        }
+        BakeGoods[] sortedGoods = new BakeGoods[count];
+        System.arraycopy(goods, 0, sortedGoods, 0, count);
+        return sortedGoods;
+    }
+
+
+
+    public BakeGoods[] searchByCountry(String bakeGoodscountry){
+    BakeGoods[] goods = new BakeGoods[size];
+    int count = 0;
+        for (int i = 0 ;i < size;i++){
+            BakeGoods item = BakeGoodsTable.get(i);
+            if (item != null && item.getOriginCountry().equals(bakeGoodscountry)) {
+                System.out.println("search successfully");
+                System.out.println(item.toString());
+                goods[count] = item;
+                count++;
+            }
+        }
+        for (int i = 1; i < count; i++) {
+            BakeGoods key = goods[i];
+            int j = i - 1;
+            while (j >= 0 && goods[j].getName().compareTo(key.getName()) > 0) {
+                goods[j + 1] = goods[j];
+                j--;
+            }
+            goods[j + 1] = key;
+        }
+        BakeGoods[] sortedGoods = new BakeGoods[count];
+        System.arraycopy(goods, 0, sortedGoods, 0, count);
+        return sortedGoods;
     }
 public String updateGoods(String Goodsname,BakeGoods newgoods){
     for (int i = 0 ;i < size;i++){
@@ -57,18 +99,19 @@ public String updateGoods(String Goodsname,BakeGoods newgoods){
     public static void main(String[] args) {
         BakeGoodsController case1 = new BakeGoodsController(10);
         BakeGoods a = new BakeGoods("a","a","a","a");
+        BakeGoods b = new BakeGoods("a","b","b","b");
         case1.addGoods(a);
-        case1.search("a");
-        case1.displayGoods();
-        case1.updateGoods("a",new BakeGoods("b","b","b","b"));
-        case1.displayGoods();
-        case1.deleteGoods(a);
-        case1.displayGoods();
-        case1.deleteGoods(a);
-        case1.displayGoods();
+        case1.addGoods(b);
+        case1.searchByName("a");
+//        case1.displayGoods();
+//        case1.updateGoods("a",new BakeGoods("b","b","b","b"));
+//        case1.displayGoods();
+//        case1.deleteGoods(a);
+//        case1.displayGoods();
+//        case1.deleteGoods(a);
+//        case1.displayGoods();
     }
 }
-
 
 
 
